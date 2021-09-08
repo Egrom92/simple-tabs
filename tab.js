df48f368668e69851390d24a8fa22fcf43a1ff03
Roman Egliens <49074979+Egrom92@users.noreply.github.com>
2021-09-08 17:38:05 +0300

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,38 +1,38 @@
-let tab;
-let tabContent;
+var tab; // заголовок вкладки
+var tabContent; // блок содержащий контент вкладки
 
 
 window.onload=function() {
-    tabContent=document.getElementsByClassName('tabContent');
-    tab=document.getElementsByClassName('tab');
-    hideTabsContent(1);
+  tabContent=document.querySelectorAll('.tabContent');
+  tab=document.querySelectorAll('.tab');
+  hideTabsContent(1);
 }
 
-document.getElementById('tabs').onclick= function (event) {
-    let target=event.target;
-    if (target.className=='tab') {
-       for (let i=0; i<tab.length; i++) {
-           if (target == tab[i]) {
-               showTabsContent(i);
-               break;
-           }
-       }
+document.querySelector('.tabs').onclick= function (event) {
+  var target=event.target;
+  if (target.className=='tab') {
+    for (var i=0; i<tab.length; i++) {
+      if (target == tab[i]) {
+        showTabsContent(i);
+        break;
+      }
     }
+  }
 }
 
 function hideTabsContent(a) {
-    for (let i=a; i<tabContent.length; i++) {
-        tabContent[i].classList.remove('show');
-        tabContent[i].classList.add("hide");
-        tab[i].classList.remove('whiteborder');
-    }
+  for (var i=a; i<tabContent.length; i++) {
+    tabContent[i].classList.remove('show');
+    tabContent[i].classList.add("hide");
+    tab[i].classList.remove('whiteborder');
+  }
 }
 
 function showTabsContent(b){
-    if (tabContent[b].classList.contains('hide')) {
-        hideTabsContent(0);
-        tab[b].classList.add('whiteborder');
-        tabContent[b].classList.remove('hide');
-        tabContent[b].classList.add('show');
-    }
+  if (tabContent[b].classList.contains('hide')) {
+    hideTabsContent(0);
+    tab[b].classList.add('whiteborder');
+    tabContent[b].classList.remove('hide');
+    tabContent[b].classList.add('show');
+  }
 }
